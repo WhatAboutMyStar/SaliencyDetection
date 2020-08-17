55ed544d85198ace62801db8c162f4b47a18c1fd
lyh <412929473@qq.com>
2020-08-17 20:21:03 +0800

    增加F-Measure

--- utils.py
+++ utils.py
@@ -10,3 +10,6 @@ def precision(y_pred, y_true):
 
 def recall(y_pred, y_true):
     return torch.mul(y_pred.round(), y_true).sum() / y_true.sum()
+
+def F_Measure(pre, rec):
+    return (1 + 0.3) * pre * rec / (0.3 * pre + rec)
